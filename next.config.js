66aae35ceed64102f775ca67b4da4a980b01fbd2
Divyanshu-Bhandari <divyanshubhandari04@gmail.com>
2025-12-27 16:56:54 +0530

    fixed errors

--- next.config.js
+++ next.config.js
@@ -9,39 +9,39 @@ const nextConfig = {
 
   async headers() {
     // 🔓 DEV MODE
-    if (isDev) {
-      return [];
-    }
-
+    // if (isDev) {
+    //   return [];
+    // }
+    return [];
     // 🔒 PROD MODE
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "script-src 'self'",
-              "style-src 'self' 'unsafe-inline'",
-              "img-src 'self' data: https:",
-              "font-src 'self' data:",
-              "connect-src 'self' https: wss://shadowtalk-ws.onrender.com",
-              "frame-ancestors 'none'",
-              "base-uri 'self'",
-              "form-action 'self'",
-            ].join('; '),
-          },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-          {
-            key: 'Permissions-Policy',
-            value: 'geolocation=(), microphone=(), camera=()',
-          },
-        ],
-      },
-    ];
+    // return [
+    //   {
+    //     source: '/:path*',
+    //     headers: [
+    //       {
+    //         key: 'Content-Security-Policy',
+    //         value: [
+    //           "default-src 'self'",
+    //           "script-src 'self'",
+    //           "style-src 'self' 'unsafe-inline'",
+    //           "img-src 'self' data: https:",
+    //           "font-src 'self' data:",
+    //           "connect-src 'self' https: wss://shadowtalk-ws.onrender.com",
+    //           "frame-ancestors 'none'",
+    //           "base-uri 'self'",
+    //           "form-action 'self'",
+    //         ].join('; '),
+    //       },
+    //       { key: 'X-Content-Type-Options', value: 'nosniff' },
+    //       { key: 'X-Frame-Options', value: 'DENY' },
+    //       { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+    //       {
+    //         key: 'Permissions-Policy',
+    //         value: 'geolocation=(), microphone=(), camera=()',
+    //       },
+    //     ],
+    //   },
+    // ];
   },
 };
 
